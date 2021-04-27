35d61e9b7afd11e03637a55329b8a2fb08fd7a47
Mike iLL Kilmer <mike@mzoo.org>
2021-04-27 17:45:12 -0500

    User logs in based on array as opposed to integer.

--- src/Access/AccessDisplay.php
+++ src/Access/AccessDisplay.php
@@ -93,7 +93,7 @@ class AccessDisplay extends Interfaces\ShortcodeScriptLoader {
 	public $has_access;
 
 	/**
-	 * Level of client access
+	 * Levels of client access
 	 *
 	 * @since  1.0.0
 	 * @access public
@@ -101,73 +101,18 @@ class AccessDisplay extends Interfaces\ShortcodeScriptLoader {
 	 * @used in handle_shortcode, localize_script
 	 * @var  array    $client_access_levels current client access levels.
 	 */
-	public $client_access_level;
+	public $client_access_levels;
 
 	/**
-	 * Level One Contracts
+	 * MBO Access Levels
 	 *
-	 * @since  1.0.0
+	 * @since  2.1.1
 	 * @access public
 	 *
 	 * @used in localize_script
-	 * @var  array    $level_1_contracts of contracts from options page.
+	 * @var  array    $mbo_access_access_levels access levels from admin/MBO.
 	 */
-	public $level_1_contracts;
-
-	/**
-	 * Level Two Contracts
-	 *
-	 * @since  1.0.0
-	 * @access public
-	 *
-	 * @used in localize_script
-	 * @var  array    $level_2_contracts of contracts from options page.
-	 */
-	public $level_2_contracts;
-
-	/**
-	 * Level Three Contracts
-	 *
-	 * @since  1.0.0
-	 * @access public
-	 *
-	 * @used in localize_script
-	 * @var  array    $level_3_contracts of contracts from options page.
-	 */
-	public $level_3_contracts;
-
-	/**
-	 * Level One Services
-	 *
-	 * @since  1.0.0
-	 * @access public
-	 *
-	 * @used in localize_script
-	 * @var  array    $level_1_services of services from options page.
-	 */
-	public $level_1_services;
-
-	/**
-	 * Level Two Services
-	 *
-	 * @since  1.0.0
-	 * @access public
-	 *
-	 * @used in localize_script
-	 * @var  array    $level_2_services of services from options page.
-	 */
-	public $level_2_services;
-
-	/**
-	 * Level Three Services
-	 *
-	 * @since  1.0.0
-	 * @access public
-	 *
-	 * @used in localize_script
-	 * @var  array    $level_3_services of services from options page.
-	 */
-	public $level_3_services;
+	public $mbo_access_levels;
 
 	/**
 	 * Handle Shortcode
@@ -182,15 +127,24 @@ class AccessDisplay extends Interfaces\ShortcodeScriptLoader {
 			array(
 				'siteid'                 => '',
 				'denied_message'         => __( 'Access to this content requires one of', 'mz-mbo-access' ),
+                // Following attr depreciated in favor of form_heading, below.
 				'call_to_action'         => __( 'Login with your Mindbody account to access this content.', 'mz-mbo-access' ),
+				'form_heading'           => __( 'Login with your Mindbody account to access this content.', 'mz-mbo-access' ),
 				'access_expired'         => __( 'Looks like your access has expired.', 'mz-mbo-access' ),
 				'denied_redirect'        => '',
-				'access_levels'          => 1,
+				'access_levels'          => [1],
+                'user_login_redirect'    => 0,
 				'manage_on_mbo'          => 'Visit Mindbody Site',
 				'password_reset_request' => __( 'Forgot My Password', 'mz-mbo-access' ),
 			),
 			$atts
 		);
+
+        // Populate Access Levels.
+        $this->mbo_access_levels = carbon_get_theme_option( 'mbo_access_access_levels' );
+
+        // Insert zero index element so indexes line up with level numbers.
+        array_unshift($this->mbo_access_levels, 'no-access');
 
 		$this->site_id = ( isset( $atts['siteid'] ) ) ? $atts['siteid'] : MZ\MZMBO()::$basic_options['mz_mindbody_siteID'];
 
@@ -326,8 +280,11 @@ class AccessDisplay extends Interfaces\ShortcodeScriptLoader {
 			'site_id'                  => $this->site_id,
 			'logged_in'                => $this->check_client_logged,
 			'has_access'               => $this->has_access,
+			'denied_redirect'          => $this->atts['denied_redirect'],
 			'denied_message'           => $this->denied_message,
 			'required_access_levels'   => $this->get_shortcode_access_levels(),
+			'all_access_levels'        => $this->mbo_access_levels,
+			'redirection_message'      => __('Redirecting...', 'mz-mbo-access'),
 		);
 
 		wp_localize_script( 'mz_mbo_access_script', 'mz_mbo_access_vars', $params );
@@ -342,10 +299,7 @@ class AccessDisplay extends Interfaces\ShortcodeScriptLoader {
      * @return array of access levels for current shortcode configuration.
      */
     private function get_shortcode_access_levels(){
-        $mbo_access_access_levels = carbon_get_theme_option( 'mbo_access_access_levels' );
-        // Insert zero index element so indexes line up with level numbers.
-        array_unshift($mbo_access_access_levels, 'no-access');
-        return array_filter($mbo_access_access_levels, function($level_key){
+        return array_filter($this->mbo_access_levels, function($level_key){
             return in_array($level_key, $this->atts['access_levels'], true);
         }, ARRAY_FILTER_USE_KEY);
     }
